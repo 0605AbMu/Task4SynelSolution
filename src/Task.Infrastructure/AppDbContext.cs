c8407715ed3fd0b35ff5f8ba9da5f644c8e51d36
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2023-01-25 14:57:41 +0500

    Initial migration created and applied to db

--- src/Task.Infrastructure/AppDbContext.cs
+++ src/Task.Infrastructure/AppDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Task.App.Entities;
 
 namespace Task.App.DataBaseContext;
 
@@ -8,5 +9,12 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Employee>()
+            .ToTable("Employees");
     }
 }
